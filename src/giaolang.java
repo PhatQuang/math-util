badad27a5f1a2827e373da9b788285a4aa4c5650
PhatQuang <phatquang1109@gmail.com>
2021-03-17 09:58:40 +0700

    1st upload to GitHub

---
+++ src/giaolang.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Phat Quang
+ */
+public class giaolang {
+    public static void main(String[] args) {
+        System.out.println("Hello gitub");
+        System.out.println("This is 1st upload connect to remote git");
+    }
+}
